cdaf9f763591ae3aad3af75abe308bac473907ce
rubens.soto <rubenssoto2005@gmail.com>
2020-04-28 11:02:08 -0300

    Criaçao do Ambiente Virtual

--- state_dimension_main.py
+++ state_dimension_main.py
@@ -5,10 +5,13 @@ from configs.start import start_spark
 
 def main():
 
-    spark = start_spark('Covid_State_Dimension')
-    data = read_csv(spark, 'us-counties.txt', True)
-    transformed_data = tr_create_state_dim(data)
+    spark = start_spark('ETL COVID')
+
+###########ETL DIMENSION STATE####################
+    data_state = read_csv(spark, 'us-counties.txt', True)
+    transformed_data = tr_create_state_dim(data_state)
     write_parquet(transformed_data, 'StateDim.parquet')
+####################################################
 
 
 if __name__ == '__main__':
